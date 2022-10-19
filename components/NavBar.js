b2364a8944450eb3938d810e79800f7548160c76
imjungim <jungim4201@gamil.com>
2022-10-19 17:26:30 +0900

    Custom App

--- components/NavBar.js
+++ components/NavBar.js
@@ -12,15 +12,13 @@ export default function NavBar() {
       <Link href="/about">
         <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
-      <style jsx>{`
-        nav {
-          background-color : tomato;
-        }
+      <style jsx >{`
+ 
         a {
           text-decoration : none;
         }
         .active {
-          color : yellow;
+          color : tomato;
         }
       `}</style>
     </nav>
@@ -31,3 +29,6 @@ export default function NavBar() {
 //코드 내 NavBar 가 active라는 이름의 클래스를 갖고 있다고 하더라도, styled jsx를 사용하기 떄문에 그 스타일은 오직 해당 컴포넌트 내부로 범위가 한정된다!
 //현재 NavBar 내 anchor 가 다른 컴포넌트의 anchor들에게 영향을 미치지 못함.
 //index.js 의 h1에 이미 사용되고 있는 클래스 이름(active)을 사용해도 클래스 이름들은 바뀌기 떄문에 작동하지 않는다.
+
+//전역으로 스타일을 적용할 경우?
+//font-size 또는 font-color와 같이.
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import NavBar from "../components/NavBar";
 
 export default function Home() {
   return <div>
-    <NavBar/>
     <h1 className="active">Hello</h1>
     </div>;
 }
