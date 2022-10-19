4427d8217a546c3fe2a0121b39102712f421fefd
jung im <jungim4201@gamil.com>
2022-10-19 16:42:37 +0900

    Styles JSX

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from './NavBar.module.css';
 
 export default function NavBar() {
   const router = useRouter();
@@ -8,13 +7,27 @@ export default function NavBar() {
   return (
     <nav>
       <Link href="/">
-        <a className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`}>Home</a>
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a className={[styles.link, router.pathname === '/about' ? styles.active : ''].join(' ')}>About</a>
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color : tomato;
+        }
+        a {
+          text-decoration : none;
+        }
+        .active {
+          color : yellow;
+        }
+      `}</style>
     </nav>
   );
 }
 
 //Link에 직접 className, style 지정 불가. a태그에 지정.
+//코드 내 NavBar 가 active라는 이름의 클래스를 갖고 있다고 하더라도, styled jsx를 사용하기 떄문에 그 스타일은 오직 해당 컴포넌트 내부로 범위가 한정된다!
+//현재 NavBar 내 anchor 가 다른 컴포넌트의 anchor들에게 영향을 미치지 못함.
+//index.js 의 h1에 이미 사용되고 있는 클래스 이름(active)을 사용해도 클래스 이름들은 바뀌기 떄문에 작동하지 않는다.
