3a24e958cb1f1450713ce47c8f62837324450b25
imjungim <jungim4201@gmail.com>
2022-10-20 22:19:37 +0900

    Redirect and Rewrite

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source : '/old-blog/:path*',
+        destination : '/new-blog/:path*', //다른 url 이동 -> 유저가 변경된 url을 인지
+        permanent : false,
+      },
+    ]
+  },
+  async rewrites() { //redirect시키지만 url은 변하지 않는다.
+    return [
+      {
+         source : "/api/movies",
+         destination : `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}` //유저가 api키를 볼 수 없음.
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,6 @@ export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
-      <span>hello~!</span>
     </Layout>
   );
 }
